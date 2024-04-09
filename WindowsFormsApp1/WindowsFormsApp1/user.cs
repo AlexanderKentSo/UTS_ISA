c35e8ea6959d38dfa7f46154b3186ac2aca03e12
Foxrama123 <163385782+Foxrama123@users.noreply.github.com>
2024-04-09 20:40:33 +0700

    User.cs
    Data member user

---
+++ WindowsFormsApp1/WindowsFormsApp1/user.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1{
+  public class user{
+    private int id;
+    private string username;
+    private string password;
+    private string alamat;
+    private string role;
+  }
+}
